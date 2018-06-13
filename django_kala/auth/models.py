8e2419eca64511eb65c0ed07af0bb59e090a340d
jessamynsmith <jessamyn.smith@gmail.com>
2018-06-13 17:31:59 -0600

    Enable superuser creation from the command line.

--- django_kala/auth/models.py
+++ django_kala/auth/models.py
@@ -12,6 +12,37 @@ import documents
 import datetime
 
 
+class KalaUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
 
@@ -32,7 +63,7 @@ class User(AbstractUser):
     removed = models.DateField(null=True)
     avatar_url = models.URLField(max_length=1200)
 
-    objects = UserManager()
+    objects = KalaUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
